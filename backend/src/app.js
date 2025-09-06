0e8314ab6a4b1e3600e7ac02259603a93a9910c8
Vimal-P-3692 <vimalp3692@gmail.com>
2025-09-06 15:52:42 +0530

    Updated ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set("trust proxy", 1);
 app.use("/api/admin", adminRoutes);
 
 module.exports = app;

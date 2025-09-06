ea018ac63f1ceddf1b77802b5fe327852cd98a57
Vimal-P-3692 <vimalp3692@gmail.com>
2025-09-06 11:45:52 +0530

    Updated ...

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -80,6 +80,7 @@ router.post("/logout", handleLogout);
 router.post("/get-dashboard", authMiddleware, getDashboard);
 
 // Routes Get
+router.get("/get-old-membership/:id", authMiddleware);
 router.get("/new-user-id", authMiddleware, getNewUserId);
 router.get("/get-list", authMiddleware, getCandidateLists);
 router.get("/progress/:id", authMiddleware, getAllProgress);
